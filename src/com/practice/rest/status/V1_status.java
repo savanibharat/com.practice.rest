c587a1187db9218c26ee0670f9c929bfb9837256
savanibharat <savanibharat@gmail.com>
2013-09-08 15:04:36 -0700

    FIRST_PROGRAM_DONE1

--- src/com/practice/rest/status/V1_status.java
+++ src/com/practice/rest/status/V1_status.java
@@ -1,5 +1,16 @@
 package com.practice.rest.status;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
 
 public class V1_status {
-
+	@GET
+	
+	@Produces(MediaType.TEXT_HTML)
+	
+	public String returnTitle()
+	{
+		return "<p>Java Web Service status</p>";
+	}
 }
